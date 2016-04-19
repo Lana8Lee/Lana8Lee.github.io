027daf20aeaa8954b98847fcfc2c99da408891be
Svetlana Lytvynenko <sveta.litvinenko1407@gmail.com>
2016-04-19 13:24:48 +0300

    final script

--- HWGoit/js_1,2/js/script.js
+++ HWGoit/js_1,2/js/script.js
@@ -1,24 +1,53 @@
 
-
 function pow(a, b) {
-        var result = a;
+    var result = a;
+    var c;
 
     for (var i = 1; i < b; i++) {
-    	result *= a;
-    }
+    	result = result * a;
+        
+    };
+    if(b > 0){
+         return result;
+    };
+    
+   if (b == 0) {
+        return 1;
+    };
+   
+ 
+if (b < 0) {
+     c = +b * (-1);
+    for(var j = 1; j < c; j++){
+        
+        result = result * a;
+    };
+    return 1 / result;
+};
+  
+     
+    
+      
+    
 
-    	return result;
-}
+};
 
+    	
 var a = prompt("Enter your number", '');
 var b = prompt("Enter your exponent", '');
+
+
+
 
    if (a % 1 == 0 && (b % 1 == 0)) {
        alert ( pow(a, b) );
 
-} else {
-	alert ("Enter an integer");
-} 
+}
+    else {
+        alert ("Enter an integer");
+    }
+	
+
     
     
     
