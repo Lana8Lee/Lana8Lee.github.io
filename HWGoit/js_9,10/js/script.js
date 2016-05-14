355792e017aede68a9c20677473a014edb5daff0
Svetlana Lytvynenko <sveta.litvinenko1407@gmail.com>
2016-05-14 18:56:47 +0300

    !

--- HWGoit/js_9,10/js/script.js
+++ HWGoit/js_9,10/js/script.js
@@ -1,22 +1,21 @@
 $(document).ready(function () {
 
-    var $links = $('.menu a');
+    //var $links = $('.menu a');
     var $bgC = $('.submenu li a');
     console.log($bgC);
 
 
-    $links.hover(function (e) {
-            e.preventDefault();
-            $(this).siblings('.submenu').slideDown(500, stop());
-        }
-        , function () {
-            $(this).siblings('.submenu').slideUp(500, stop());
-        });
-
-    function stop() {
-        //$(".sub-menu").stop(true, true);
-        $(this).siblings('.submenu').stop(true, true);
-    };
+    $('.dropdown').hover(
+function(){
+$(this).children('.submenu').slideDown(500,stop()); 
+},
+function(){
+$(this).children('.submenu').slideUp(500,stop()); 
+}
+);
+function stop(){
+$('.submenu').stop(true, true);
+}
 
 
 
